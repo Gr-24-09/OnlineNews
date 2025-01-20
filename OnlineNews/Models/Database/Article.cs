7a72ecc3c7ffd13e554dbf0c65d9b391c82d20e5
Madhuri8247 <madhurimandalapu82473@gmail.com>
2025-01-20 14:41:58 +0100

    Updated class Article class,User class

---
+++ OnlineNews/Models/Database/Article.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineNews.Models.Database
+{
+    public class Article
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title of Article is required.")]
+        [Display(Name = "Title of Article")]
+        [StringLength(100)]
+        public string Headline { get; set; }
+
+        [Required(ErrorMessage = "Content of Article is required.")]
+        [Display(Name = "Content of Article")]
+        public string Content { get; set; }
+
+        [Required(ErrorMessage = "ContentSummary of Article is required.")]
+        [Display(Name = "ContentSummary of Article")]
+        public string ContentSummary { get; set; }
+
+        [Required(ErrorMessage = "Category of Article is required.")]
+        [Display(Name = "Category")]
+        [StringLength(50)]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Publishing Date of Article is required.")]
+        [Display(Name = "Publishing Date")]
+        public DateTime DateStamp { get; set; }
+
+        [Required(ErrorMessage = "Name of the Writer is required.")]
+        [Display(Name = "Writer Name")]
+        [StringLength(50)]
+        public string Writer { get; set; }
+
+        [Required(ErrorMessage = "Name of Location is required.")]
+        [Display(Name = "Location Name")]
+        [StringLength(50)]
+        public string Location { get; set; }
+        public string ImageLink { get; set; } = string.Empty;
+
+    }
+}
---
+++ OnlineNews/Models/Database/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineNews.Models.Database
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name of Employee is required.")]
+        [Display(Name = "Name of Employee")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName of Employee is required.")]
+        [Display(Name = "LastName of Employee")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+
+        [Required(ErrorMessage = "LastName of Employee is required.")]
+        public DateTime DateofBirth { get; set; }
+       
+        
+
+    }
+}
